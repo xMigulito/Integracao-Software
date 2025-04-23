deeb8de25d5c8d25414f0229a97027d3847533e9
xMigulito <125411842+xMigulito@users.noreply.github.com>
2025-04-23 01:28:40 -0300

    Save

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -5,7 +5,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    console.log('DATABASE_URL:', process.env.DATABASE_URL);
     super();
   }
 
--- src/eventos/dto/update-evento.dto.ts
+++ src/eventos/dto/update-evento.dto.ts
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEventoDto } from './create-evento.dto';
 import { IsDate, IsString } from 'class-validator';
